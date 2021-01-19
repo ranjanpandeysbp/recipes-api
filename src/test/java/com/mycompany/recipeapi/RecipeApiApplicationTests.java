3d2a417f3fc2aa5a8cb753b9c91ce403503a8cd2
RP <rp@g.com>
2021-01-19 22:16:21 +0530

    spring initializer project setup

---
+++ src/test/java/com/mycompany/recipeapi/RecipeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.recipeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
