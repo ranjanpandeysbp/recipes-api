31f733aa7a4b97728b0cc23784a0609f54a4c906
Ranjan Pandey <ranjanpandey9630@gmail.com>
2021-01-20 22:30:58 +0530

    added ingredient controller

---
+++ src/main/java/com/mycompany/recipeapi/controller/IngredientController.java
@@ -0,0 +1,23 @@
+package com.mycompany.recipeapi.controller;
+
+import com.mycompany.recipeapi.entity.Ingredient;
+import com.mycompany.recipeapi.repository.IngredientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/v1")
+public class IngredientController {
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @GetMapping("/ingredients")
+    public Iterable<Ingredient> getAllIngredients(){
+        return ingredientRepository.findAll();
+    }
+}
